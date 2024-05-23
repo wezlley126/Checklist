ca2fb00c89f859fa2adc7ab04140ad896b984dbc
Weslley Borges <84255451+wezlley126@users.noreply.github.com>
2024-05-23 11:44:28 -0300

    Update dropTasks.js

--- components/dropTasks.js
+++ components/dropTasks.js
@@ -5,6 +5,6 @@ export default async function DeleteTask(e) {
     const dataSend = {
         idTask: e.target.value
     }
-    const dropRequest = await axios.post('http://localhost:3000/api/tasks/delete', dataSend)
+    const dropRequest = await axios.post('/api/tasks/delete', dataSend)
     console.log('Dado a ser enviado --------->', e.target.value)
   }
